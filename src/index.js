128e316c40f4448a608fbbab8985ee4e4e257df2
Boocie-N <contact.kesed@gmail.com>
2023-04-16 14:48:15 +0200

    added current city, temp, speed, humidity and real-feel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+function displayTemp(response) {
+  console.log(response.data);
+  let temperature = document.querySelector("#main-temp");
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.city;
 
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.condition.description;
+
+  let realFeel = document.querySelector("#real-feel");
+  realFeel.innerHTML = Math.round(response.data.temperature.feels_like);
+
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.temperature.humidity;
+
+  let windSpeed = document.querySelector("#wind-speed");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "33b2dd0bdtf63faf92eoc3485e96bfca";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=paris&key=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemp);
